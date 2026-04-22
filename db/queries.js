720fa7b4fb58328059fe9dc79c28874e42e5e6f4
R3ndrex <yacholohchtoli@gmail.com>
2026-04-22 22:33:21 +0300

    add givemembership function

--- db/queries.js
+++ db/queries.js
@@ -12,6 +12,15 @@ async function findUserByEmail(email) {
     );
     return rows[0];
 }
+async function giveMembership(userId) {
+    await db.query(
+        `UPDATE users
+        SET status='member'
+        WHERE users.id=$1`,
+        [userId],
+    );
+}
+
 async function createPost({ title, description, authorId }) {
     await db.query(
         `INSERT INTO posts(title,description,createdAt,authorId) VALUES($1,$2,NOW(),$3)`,
@@ -31,7 +40,7 @@ async function getAllPosts() {
 }
 async function createUser({ lastName, firstName, email, password }) {
     const { rows } = await db.query(
-        `INSERT INTO users(firstName,lastName,email,password,status) VALUES($1,$2,$3,$4,'member') RETURNING id, firstName, lastName, email, status`,
+        `INSERT INTO users(firstName,lastName,email,password,status) VALUES($1,$2,$3,$4,'user') RETURNING id, firstName, lastName, email, status`,
         [firstName, lastName, email, password],
     );
     return rows[0];
@@ -41,5 +50,6 @@ module.exports = {
     createUser,
     findUserByEmail,
     createPost,
+    giveMembership,
     getAllPosts,
 };
